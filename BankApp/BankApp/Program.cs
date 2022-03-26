03d1d31d47e17e5c3a4a45372dd3518503968f47
sahinomeerr <102425302+sahinomeerr@users.noreply.github.com>
2022-03-26 18:45:50 +0300

    First
    created the project

---
+++ BankApp/BankApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BankApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
